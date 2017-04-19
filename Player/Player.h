0618ce074144592bf74687d77b5304e075d09da3
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-04-19 13:14:54 +0200

    EMP-9391 Basic project structure

---
+++ Player/Player.h
@@ -0,0 +1,19 @@
+//
+//  Player.h
+//  Player
+//
+//  Created by Fredrik Sjöberg on 2017-04-19.
+//  Copyright © 2017 emp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Player.
+FOUNDATION_EXPORT double PlayerVersionNumber;
+
+//! Project version string for Player.
+FOUNDATION_EXPORT const unsigned char PlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Player/PublicHeader.h>
+
+
