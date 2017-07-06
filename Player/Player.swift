892701802a9e5464fb10474db658625812874637
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-07-06 21:21:27 +0200

    EMP-9595 Insignificant change to trigger update

--- Player/Player.swift
+++ Player/Player.swift
@@ -17,6 +17,8 @@ public final class Player {
     public init() {
         avPlayer = AVPlayer()
         
+        
+        
         playerObserver.observe(path: .currentItem, on: avPlayer) { [unowned self] player, change in
             print("Player.currentItem changed",player, change.new)
         }
