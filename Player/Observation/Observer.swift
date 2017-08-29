0d537c293f36f4fc90088af717e9a95e4e5e2430
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-08-29 10:44:00 +0200

    EMP-10210 Final observation docs

--- Player/Observation/Observer.swift
+++ Player/Observation/Observer.swift
@@ -8,11 +8,18 @@
 
 import Foundation
 
-class Observer<T: NSObject>: NSObject {
-    unowned var object: T
-    let path: String
-    var callback: (T, KVOChange) -> Void = { _ in }
-    init(of object: T, at path: String, with options: NSKeyValueObservingOptions, callback: @escaping (T, KVOChange) -> Void) {
+/// `Observer`s represent the *observable* used to track a registered `KVO` subscriber
+internal class Observer<T: NSObject>: NSObject {
+    /// Target to observe
+    internal unowned var object: T
+    
+    /// *KeyPath* on `object` to observe
+    internal let path: String
+    
+    /// Fires when the `KVO` fires.
+    internal var callback: (T, KVOChange) -> Void = { _ in }
+    
+    internal init(of object: T, at path: String, with options: NSKeyValueObservingOptions, callback: @escaping (T, KVOChange) -> Void) {
         self.object = object
         self.path = path
         self.callback = callback
@@ -24,11 +31,15 @@ class Observer<T: NSObject>: NSObject {
                            context: nil)
     }
     
-    func cancel() {
+    /// Stops `KVO` observation of `object` at `path`
+    internal func cancel() {
         object.removeObserver(self, forKeyPath: path)
     }
     
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    /// Override that triggers the `callback` when a `KVO` change is observed.
+    ///
+    /// Please see Apple's documentation regarding *Key Value Observation* for more information.
+    internal override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         guard path == keyPath else { return }
         guard self.object == object as? T else { return }
         
