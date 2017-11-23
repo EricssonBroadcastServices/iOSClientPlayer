b3a7a1c0b176e8ba944a3b2c85884945bdfa5f5a
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-11-23 13:11:08 +0100

    EMP-10650 More documentation

--- Player/Tech/Tech.swift
+++ Player/Tech/Tech.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
+/// A `PlaybackTech` is defined as a technology through which playback in a specified `Context` can occur.
 public protocol PlaybackTech: class {
+    /// Error specific to the `Tech`
     associatedtype TechError: Error
+    
+    /// Specifies the required data to configure the `Tech`.
+    ///
+    /// This could include `DRM` agents, `url`, app tokens or `Tech` specific environment variables
     associatedtype Configuration
+    
+    /// The `MediaContext` defining the playback. This can be a generic context or one stricly tied to the `Tech`
     associatedtype Context: MediaContext
     
+    /// Used by `Player` to register event callbacks.
+    ///
+    /// `Tech`s are expected to trigger defined events according to their specification.
     var eventDispatcher: EventDispatcher<Context, Self> { get }
-    
-//    func prepare(callback: @escaping (TechError?) -> Void)
 }
 
